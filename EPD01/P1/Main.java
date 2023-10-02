b6e3ff5bc7a2430fac3ba422bbc40ae941f37238
vballeo <vicballesle@gmail.com>
2023-10-02 04:26:57 +0200

    Esto ha sido hecho en una clase de github

--- EPD01/P1/Main.java
+++ EPD01/P1/Main.java
@@ -5,6 +5,6 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola clase de EPD 1");
+        System.out.println("Esto ha sido hecho en una clase de github");
     }
 }
